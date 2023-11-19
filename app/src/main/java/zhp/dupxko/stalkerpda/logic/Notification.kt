26c57318599653fc5999c387b9ca20da6f6469dc
Yebigun <grzechu982@gmail.com>
2023-11-19 20:11:10 +0100

    a ja mam to w fiacie (nie działa jakiś bullshit camerowy lol)

--- app/src/main/java/zhp/dupxko/stalkerpda/logic/Notification.kt
+++ app/src/main/java/zhp/dupxko/stalkerpda/logic/Notification.kt
@@ -1,59 +1,70 @@
 package zhp.dupxko.stalkerpda.logic
 
-import android.app.ForegroundServiceStartNotAllowedException
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.Service
+import android.Manifest
+import android.app.*
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.content.pm.ServiceInfo
 import android.os.Build
 import android.os.IBinder
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
-import androidx.core.app.ServiceCompat
-import androidx.core.content.ContextCompat
-import zhp.dupxko.stalkerpda.Manifest
+import androidx.core.app.NotificationManagerCompat
+import zhp.dupxko.stalkerpda.MainActivity
 import zhp.dupxko.stalkerpda.R
 
 class Notification : Service() {
 
-    private fun startForeground() {
-        // Before starting the service as foreground check that the app has the
-        // appropriate runtime permissions. In this case, verify that the user has
-        // granted the CAMERA permission.
-        val cameraPermission =
-            ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
-        if (cameraPermission == PackageManager.PERMISSION_DENIED) {
-            // Without camera permissions the service cannot run in the foreground
-            // Consider informing user or updating your app UI if visible.
-            stopSelf()
-            return
-        }
+    private val CHANNEL_ID = "diary_eneterance"
+    private val notificationId = 111
 
-        try {
-            val notification = NotificationCompat.Builder(this, "CHANNEL_ID")
-                // Create the notification to display while the service is running
-                .build()
-            ServiceCompat.startForeground(
-                /* service = */ this,
-                /* id = */ 100, // Cannot be 0
-                /* notification = */ notification,
-                /* foregroundServiceType = */
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                    ServiceInfo.FOREGROUND_SERVICE_TYPE_CAMERA
-                } else {
-                    0
-                },
-            )
-        } catch (e: Exception) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
-                && e is ForegroundServiceStartNotAllowedException
-            ) {
-                // App not in a valid state to start foreground service
-                // (e.g. started from bg)
+    override fun onBind(p0: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+
+    fun createNotificationChannel(a: String?) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "a"
+            val descriptionText = "aaa"
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
             }
-            // ...
+
+        val notificationManager: NotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+
         }
     }
+
+    private fun sendNotification() {
+
+
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("Title")
+            .setContentText("description")
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+//          to chyba do main
+//        with(NotificationManagerCompat.from()){
+//            if (ActivityCompat.checkSelfPermission(
+//                    this,
+//                    Manifest.permission.POST_NOTIFICATIONS
+//                ) != PackageManager.PERMISSION_GRANTED
+//            ) {
+//                // TODO: Consider calling
+//                //    ActivityCompat#requestPermissions
+//                // here to request the missing permissions, and then overriding
+//                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//                //                                          int[] grantResults)
+//                // to handle the case where the user grants the permission. See the documentation
+//                // for ActivityCompat#requestPermissions for more details.
+//                return
+//            }
+//            notify(notificationId, builder.build())
+//        }
+
+    }
+
 }
