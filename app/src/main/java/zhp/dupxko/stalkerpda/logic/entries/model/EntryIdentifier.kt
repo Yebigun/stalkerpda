ad443ecb1057d5c0ab0456ee4f8782f46f40f329
hvk1444 <shadowteam99@gmail.com>
2024-01-04 11:47:19 +0100

    convert java enum to kt enum

--- app/src/main/java/zhp/dupxko/stalkerpda/logic/entries/model/EntryIdentifier.kt
+++ app/src/main/java/zhp/dupxko/stalkerpda/logic/entries/model/EntryIdentifier.kt
@@ -1,14 +1,6 @@
-package zhp.dupxko.stalkerpda.logic.entries.model;
+package zhp.dupxko.stalkerpda.logic.entries.model
 
 //TODO add more info about entries when we know anything more about them
-public enum EntryIdentifier {
-    ENTRY_1,
-    ENTRY_2,
-    ENTRY_3,
-    ENTRY_4,
-    ENTRY_5,
-    ENTRY_6,
-    ENTRY_7,
-    ENTRY_8,
-    ENTRY_9,
+enum class EntryIdentifier {
+    ENTRY_1, ENTRY_2, ENTRY_3, ENTRY_4, ENTRY_5, ENTRY_6, ENTRY_7, ENTRY_8, ENTRY_9
 }
