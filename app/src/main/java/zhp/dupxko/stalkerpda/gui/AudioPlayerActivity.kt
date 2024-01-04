07e5539598bf911fdb73eede19576fe4198c5e57
hvk1444 <shadowteam99@gmail.com>
2024-01-04 21:13:01 +0100

    use audio player to play cool songs (and in the future, perhaps even the diary entries)

--- app/src/main/java/zhp/dupxko/stalkerpda/gui/AudioPlayerActivity.kt
+++ app/src/main/java/zhp/dupxko/stalkerpda/gui/AudioPlayerActivity.kt
@@ -1,25 +1,40 @@
 package zhp.dupxko.stalkerpda.gui
 
+import android.annotation.SuppressLint
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.media3.common.MediaItem
+import androidx.media3.datasource.RawResourceDataSource
 import androidx.media3.ui.PlayerView
 import zhp.dupxko.stalkerpda.R
 import zhp.dupxko.stalkerpda.logic.audioplayer.PlayerService
 
 class AudioPlayerActivity : AppCompatActivity() {
-
     private lateinit var playerView: PlayerView
     private lateinit var playerService: PlayerService
 
+    @SuppressLint("UnsafeOptInUsageError")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_audio_player)
-
         //get the view
         playerView = findViewById(R.id.player_view)
         // Instantiate the player.
         playerService = PlayerService(this)
         // Attach player to the view.
-        playerView.player = playerService.player
+        val player = playerService.player
+        playerView.player = player
+
+        val audioId: Int = intent.getIntExtra("testuwa", 0)
+// Build the media item.
+        val uri = RawResourceDataSource.buildRawResourceUri(audioId)
+        val mediaItem = MediaItem.fromUri(uri)
+// Set the media item to be played.
+        player.setMediaItem(mediaItem)
+// Prepare the player.
+        player.prepare()
+// Start the playback.
+        player.play()
     }
 }
--- app/src/main/java/zhp/dupxko/stalkerpda/gui/MainActivity.kt
+++ app/src/main/java/zhp/dupxko/stalkerpda/gui/MainActivity.kt
@@ -14,7 +14,9 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import androidx.core.app.ActivityCompat
+import androidx.navigation.fragment.findNavController
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import zhp.dupxko.stalkerpda.R
@@ -27,22 +29,12 @@ class MainActivity : AppCompatActivity() {
     private var latitude: Double? = null
 
     private lateinit var fusedLocationClient: FusedLocationProviderClient       //lokalizacja
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setSupportActionBar(binding.toolbar)
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 
     override fun onStart() {
@@ -66,28 +58,12 @@ class MainActivity : AppCompatActivity() {
             startService(it)
         }
 
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+        val intent = Intent(this, AudioPlayerActivity::class.java)
+        intent.putExtra("testuwa", R.raw.expodchorazy)
+        binding.button2.setOnClickListener {
+            startActivity(intent)
         }
-    }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
     }
 
     fun getLastKnownLocation() {
