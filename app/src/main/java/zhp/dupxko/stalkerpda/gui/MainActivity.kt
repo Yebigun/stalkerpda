ae3f82cb327570cda0d470b514d6352b5685e0b3
Yebigun <grzechu982@gmail.com>
2024-01-08 17:08:51 +0100

    added localization read button

--- app/src/main/java/zhp/dupxko/stalkerpda/gui/MainActivity.kt
+++ app/src/main/java/zhp/dupxko/stalkerpda/gui/MainActivity.kt
@@ -6,17 +6,8 @@ import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
 import androidx.core.app.ActivityCompat
-import androidx.navigation.fragment.findNavController
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import zhp.dupxko.stalkerpda.R
@@ -25,14 +16,12 @@ import zhp.dupxko.stalkerpda.logic.LocationForegroundService
 
 class MainActivity : AppCompatActivity() {
 
-    private var longitude: Double? = null
-    private var latitude: Double? = null
-
     private lateinit var fusedLocationClient: FusedLocationProviderClient       //lokalizacja
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
@@ -58,32 +47,43 @@ class MainActivity : AppCompatActivity() {
             startService(it)
         }
 
-        val intent = Intent(this, AudioPlayerActivity::class.java)
-        intent.putExtra("testuwa", R.raw.expodchorazy)
-        binding.button2.setOnClickListener {
-            startActivity(intent)
-        }
+        bindDeatils()
+
 
     }
 
-    fun getLastKnownLocation() {
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
+    fun bindDeatils() {
+        val intent = Intent(this, AudioPlayerActivity::class.java)
+        intent.putExtra("testuwa", R.raw.expodchorazy)
+
+        binding.button2.setOnClickListener {
+            startActivity(intent)
+        }
+        binding.buttonGps.setOnClickListener {
+            getLastKnownLocation()
+        }
+    }
+
+    private fun getLastKnownLocation() {
+
+        if (
+            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions( this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 100);
             return
         }
-        fusedLocationClient.lastLocation
-            .addOnSuccessListener { location->
-                if (location != null) {
-                    latitude =  location?.latitude
-                    longitude = location?.longitude
-                }
-
+        val location = fusedLocationClient.lastLocation
+        binding.button2.text
+        location.addOnSuccessListener {
+            if (it != null) {
+                val textAccuracy = it.accuracy.toString()
+                val textLatitude =  it.latitude.toString()
+                val textLongitude = it.longitude.toString()
+                binding.longitude.text = textAccuracy
+                binding.latitude.text = textLatitude
+                Log.i("localization","$textLatitude $textLongitude $textAccuracy ${it.hasAccuracy()}")
             }
+        }
     }
 }
